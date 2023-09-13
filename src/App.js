9c865fc11f02e099f05c175e2be9c52e405ef463
Amandeep112 <103934091+Amandeep112@users.noreply.github.com>
2023-09-13 14:38:19 +0530

    Updte App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
+    // if (/Safari/i.test(navigator.userAgent)) {
+    //   window.open("https://tbsecomd.wpengine.com/openApp");
+    // } else {
     var webURL;
     if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
       webURL =
@@ -11,18 +14,15 @@ function App() {
     } else {
       webURL = `market://details?id=com.beerstore`;
     }
+
     var isMobile =
       /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
         navigator.userAgent
       );
-    var safariExpression = /Safari/i.test(navigator.userAgent);
-    // var deepLinkURL = safariExpression
-    //   ? "https://tbsecomd.wpengine.com/openApp"
-    //   : `thebeerstore://`;
+
+    var deepLinkURL = `thebeerstore://`;
     if (isMobile) {
-      safariExpression
-        ? window.open("https://tbsecomd.wpengine.com/openApp", "_blank")
-        : (window.location.href = `thebeerstore://`);
+      window.location.href = deepLinkURL;
       setTimeout(function () {
         // If the app does not open, fall back to the browser
         window.open(webURL, "_blank").focus();
@@ -33,18 +33,10 @@ function App() {
       window.open("https://www.thebeerstore.ca/", "_blank");
       window.location.href = "https://www.thebeerstore.ca/";
     }
+    // }
   }, []);
 
-  return (
-    <div className="App">
-      <iframe
-        style={{ display: "none" }}
-        height="0"
-        width="0"
-        id="loader"
-      ></iframe>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
